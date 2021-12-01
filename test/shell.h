2905eb6f102a6512d2711ee9886163f9a568260a
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-12-01 08:10:13 -0500

    Add _strlen, _strcat and path_cmd functions

--- test/shell.h
+++ test/shell.h
@@ -5,10 +5,15 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <fcntl.h>
 #include <sys/wait.h>
+#include <sys/syscall.h>
 
+int _strlen(char *string);
+char *_strcat(char *dest, char *src);
 char **parse_line(char *line);
-char **tokenize_path(char *argv_0);
+char **tokenize_path(void);
+char *path_cmd(char **path_tokens, char *cmd);
 int execute(char **arguments);
 
 #endif /* SHELL_H */
