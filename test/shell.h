3d6840dd413cfa14fbc45a3f1c869bf1844b6055
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-12-01 16:10:43 -0500

    Delete tokenize_path function

--- test/shell.h
+++ test/shell.h
@@ -11,9 +11,9 @@
 
 int _strlen(char *string);
 char *_strcat(char *dest, char *src);
+char *_strcpy(char *arguments, char *if_comand);
 char **parse_line(char *line);
-char **tokenize_path(void);
-char *path_cmd(char **path_tokens, char *cmd);
+char *path_cmd(char *cmd);
 int execute(char **arguments);
 
 #endif /* SHELL_H */
