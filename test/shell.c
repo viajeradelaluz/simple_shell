b71997b212ed28040cf015431de7ec1c37b33f67
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-12-01 08:17:07 -0500

    Add path tokenized to parse

--- test/shell.c
+++ test/shell.c
@@ -6,8 +6,8 @@ int main(int argc, char **argv)
 	(void)argv;
 
 	int terminal = 1, read;
-	char *line = NULL;
-	char **arguments = NULL;
+	char *line = NULL, *if_command = NULL;
+	char **arguments = NULL, **path_tokenized = NULL;
 	size_t size_line;
 
 	/* Check if FD is a terminal */
@@ -30,11 +30,24 @@ int main(int argc, char **argv)
 
 		/* Parse and Execute */
 		arguments = parse_line(line);
+		path_tokenized = tokenize_path();
+
+		if (access(arguments[0], F_OK) != 0)
+		{
+			if_command = path_cmd(path_tokenized, arguments[0]);
+			if (!if_command)
+				return (-1);
+			arguments[0] = if_command;
+		}
 		execute(arguments);
 
 	} while (1);
 
 	free(line);
+	free(if_command);
 	free(arguments);
+	free(path_tokenized);
 	return (0);
 }
+/* Thank you! */
+/*Daniel Was Here*/ /*Daniel Left*/
