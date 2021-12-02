5f356391072d090f514e1fdabde8777fbdf25432
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-12-01 19:22:33 -0500

    Add _strconcat function / Fix _strlen function

--- test/read_functions.c
+++ test/read_functions.c
@@ -20,14 +20,37 @@ char *_strcat(char *dest, char *src)
 	return (dest);
 }
 
+char *_strconcat(char *str_1, char *str_2)
+{
+	int size_str_1 = _strlen(str_1);
+	int size_str_2 = _strlen(str_2);
+	char *p = NULL;
+	char *copy = malloc(sizeof(char *) * (size_str_1 + size_str_2 + 1));
+
+	if (!copy)
+		return (NULL);
+
+	p = copy;
+	while (*str_1)
+		*p++ = *str_1++;
+
+	while (*str_2)
+		*p++ = *str_2++;
+
+	*p = '\0';
+
+	return (copy);
+}
+
 /**
  *
  */
+
 int _strlen(char *string)
 {
 	int counter = 0;
 
-	while (string)
+	while (*(string + counter))
 		counter++;
 
 	return (counter);
@@ -38,21 +61,20 @@ int _strlen(char *string)
  */
 char *_strcpy(char *arguments, char *if_comand)
 {
-    int size_if_comand = _strlen(if_comand);
-    int counter = 0;
+	int size_if_comand = _strlen(if_comand);
+	int counter = 0;
 
-    arguments = malloc(sizeof(char *) * (size_if_comand + 1));
+	arguments = malloc(sizeof(char *) * (size_if_comand + 1));
 
-    if (!arguments)
-        return (NULL);
+	if (!arguments)
+		return (NULL);
 
-    while (if_comand[counter])
-    {
-        arguments[counter] = if_comand[counter];
-        counter++;
-    }
-    arguments[counter] = if_comand[counter];
+	while (if_comand[counter])
+	{
+		arguments[counter] = if_comand[counter];
+		counter++;
+	}
+	arguments[counter] = if_comand[counter];
 
-	free(arguments);
-    return (arguments);
+	return (arguments);
 }
