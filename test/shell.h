62a44011707b76644d8911047cbeb1efed298f03
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-12-01 19:20:25 -0500

    Add _strconcat function

--- test/shell.h
+++ test/shell.h
@@ -11,6 +11,7 @@
 
 int _strlen(char *string);
 char *_strcat(char *dest, char *src);
+char *_strconcat(char *str_1, char *srt_2);
 char *_strcpy(char *arguments, char *if_comand);
 char **parse_line(char *line);
 char *path_cmd(char *cmd);
