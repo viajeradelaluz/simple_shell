be13d57fe4befbc723c6b3997b044fcb25ced578
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-12-01 19:21:22 -0500

    Fix path_cmd function calling

--- test/shell.c
+++ test/shell.c
@@ -1,13 +1,14 @@
 #include "shell.h"
 
-int main(int argc, char **argv)
+int main(int argc, char **argv, char **env)
 {
 	(void)argc;
 	(void)argv;
+	(void)env;
 
 	int terminal = 1, read;
 	char *line = NULL, *if_command = NULL;
-	char **arguments = NULL, **path_tokenized = NULL;
+	char **arguments = NULL;
 	size_t size_line;
 
 	/* Check if FD is a terminal */
@@ -17,9 +18,9 @@ int main(int argc, char **argv)
 		terminal = 0;
 	}
 
-	/* Read */
 	do
 	{
+		/* Read */
 		if (terminal == 1)
 			write(STDOUT_FILENO, "($) ", 4);
 
@@ -30,24 +31,18 @@ int main(int argc, char **argv)
 
 		/* Parse and Execute */
 		arguments = parse_line(line);
-		path_tokenized = tokenize_path();
 
-		if (access(arguments[0], F_OK) != 0)
-		{
-			if_command = path_cmd(path_tokenized, arguments[0]);
-			if (!if_command)
-				return (-1);
-			arguments[0] = if_command;
-		}
+		if_command = path_cmd(arguments[0]);
+		if (!if_command)
+			return (-1);
+
+		arguments[0] = if_command;
+
 		execute(arguments);
 
 	} while (1);
 
 	free(line);
-	free(if_command);
 	free(arguments);
-	free(path_tokenized);
 	return (0);
 }
-/* Thank you! */
-/*Daniel Was Here*/ /*Daniel Left*/
