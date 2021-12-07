27153afa2f00f22354b2fc0f103d7a0c59d0d975
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-12-07 09:38:43 -0500

    Update strtok calling

--- parse.c
+++ parse.c
@@ -14,13 +14,13 @@ char **tokenize_line(char *line)
 	if (!tokens)
 		return (NULL);
 
-	token = _strtok(line, " \t\r\n\a");
+	token = strtok(line, " \t\r\n\a");
 	while (token)
 	{
 		tokens[position] = token;
 		position++;
 
-		token = _strtok(NULL, " \t\r\n\a");
+		token = strtok(NULL, " \t\r\n\a");
 	}
 	tokens[position] = NULL;
 
@@ -30,13 +30,13 @@ char **tokenize_line(char *line)
 
 /**
  * tokenize_path - Tokenize a the PATH of a Unix-SO.
- * @void: no values to evaluate.
+ * @path: string with PATH (environ-variable).
  * Return: an array of strings with tokens-PATH directories.
  */
-char **tokenize_path(void)
+char **tokenize_path(char *path)
 {
 	int position = 0;
-	char *path = _getPATH(), *path_line = NULL;
+	char *path_line = NULL;
 	char **path_tokens = malloc(sizeof(char *) * 1024);
 
 	if (!path_tokens)
@@ -45,12 +45,12 @@ char **tokenize_path(void)
 		return (NULL);
 	}
 
-	path_line = _strtok(path, ":");
+	path_line = strtok(path, ":");
 	while (path_line)
 	{
 		path_tokens[position] = path_line;
 		position++;
-		path_line = _strtok(NULL, ":");
+		path_line = strtok(NULL, ":");
 	}
 	path_tokens[position] = NULL;
 
@@ -60,14 +60,15 @@ char **tokenize_path(void)
 
 /**
  * path_cmd - Search a command into the PATH directories.
+ * @path: string with PATH (environ-variable).
  * @cmd: command to find in PATH.
  * Return: a string with the command and its PATH.
  */
-char *path_cmd(char *cmd)
+char *path_cmd(char *path, char *cmd)
 {
 	int position = 0;
 	char *temporal = NULL;
-	char **path_tokens = tokenize_path();
+	char **path_tokens = tokenize_path(path);
 
 	while (path_tokens[position])
 	{
