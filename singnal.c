1fc5e96643e20bf180a5b1c492655fb8bdac0faa
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-12-08 19:16:08 -0500

    Fix betty-style

--- singnal.c
+++ singnal.c
@@ -7,26 +7,26 @@
  */
 void sigint_c(int signal)
 {
-    (void)signal;
-    write(STDOUT_FILENO, "\n($) ", 5);
+	(void)signal;
+	write(STDOUT_FILENO, "\n($) ", 5);
 }
 
 /**
- * check_comment - Checks if the line read by get line contains the # to reemplace for  '\0'
+ * check_comment - Checks if the line contains the # to reemplace for '\0'
  * @line: Line read by getline to check
  * Return: void
  */
 void check_comment(char *line)
 {
-    int i = 0;
+	int i = 0;
 
-    while(*(line + i))
-    {
-        if (*(line + i) == 35)
-        {
-            *(line + i) = '\0';
-            break;
-        }
-        i++;
-    }
+	while (*(line + i))
+	{
+		if (*(line + i) == 35)
+		{
+			*(line + i) = '\0';
+			break;
+		}
+		i++;
+	}
 }
